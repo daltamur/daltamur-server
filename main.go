fab119d64a489e5726a4b5555227e42e32084850
daltamur <daltamur@oswego.edu>
2022-11-30 15:37:42 -0500

    ok we do need the garbage collection call

--- main.go
+++ main.go
@@ -551,7 +551,7 @@ func filterTwoDays(t *time.Time, t2 *time.Time, writer http.ResponseWriter) {
 
 	jsonBytes = nil
 	allDays = nil
-	debug.FreeOSMemory()
+	runtime.GC()
 }
 
 func getSingleDayVals(t time.Time, day string) DaySongs {
